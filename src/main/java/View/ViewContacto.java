7f633a46796bd3f53fc4cac1db80fc10bc71aec2
Manfred Armijos <Manfred Armijos@ALMM>
2026-02-06 01:45:16 -0500

    view

---
+++ src/main/java/View/ViewContacto.java
@@ -0,0 +1,109 @@
+package View;
+
+import javax.swing.JButton;
+
+public class ViewContacto extends javax.swing.JFrame {
+
+    /**
+     * Creates new form ViewBancaMovil
+     */
+    public ViewContacto() {
+        initComponents();
+        setUndecorated(true);
+        setLocationRelativeTo(null);
+    }
+    
+
+    public String getUsername() {
+        return txfUsername.getText();
+    }
+    
+    public void showErrors(String error){
+        lblErrors.setText(error);
+    }
+    
+    public void clear(){
+        lblErrors.setText("");
+        txfUsername.setText("");
+    }
+    
+
+    public JButton getBtnExit() {
+        return btnExit;
+    }
+
+    public JButton getBtnLogin() {
+        return btnLogin;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        txfUsername = new javax.swing.JTextField();
+        btnLogin = new javax.swing.JButton();
+        lblErrors = new javax.swing.JLabel();
+        btnExit = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(414, 896));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        txfUsername.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        txfUsername.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txfUsername.setBorder(null);
+        txfUsername.setPreferredSize(new java.awt.Dimension(500, 75));
+        jPanel1.add(txfUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 250, 200, 40));
+
+        btnLogin.setBackground(new java.awt.Color(10, 107, 207));
+        btnLogin.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        btnLogin.setBorder(null);
+        btnLogin.setBorderPainted(false);
+        btnLogin.setContentAreaFilled(false);
+        btnLogin.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnLogin.setFocusPainted(false);
+        jPanel1.add(btnLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 790, 350, 60));
+
+        lblErrors.setForeground(new java.awt.Color(255, 158, 0));
+        lblErrors.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jPanel1.add(lblErrors, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 360, 350, 20));
+
+        btnExit.setBackground(new java.awt.Color(10, 107, 207));
+        btnExit.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        btnExit.setBorder(null);
+        btnExit.setBorderPainted(false);
+        btnExit.setContentAreaFilled(false);
+        btnExit.setFocusPainted(false);
+        btnExit.setFocusable(false);
+        btnExit.setPreferredSize(new java.awt.Dimension(45, 45));
+        jPanel1.add(btnExit, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, 40, 40));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/iconSelect.png"))); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnExit;
+    private javax.swing.JButton btnLogin;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblErrors;
+    private javax.swing.JTextField txfUsername;
+    // End of variables declaration//GEN-END:variables
+}
